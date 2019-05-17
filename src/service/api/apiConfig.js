aeef9420e9fe700c29b2eb9c505108fa42958c8d
Andrew Sutherland <azsuth@gmail.com>
2019-05-17 16:12:39 -0500

    removed scheme and host from base URL, hopefully they're inherited???

--- src/service/api/apiConfig.js
+++ src/service/api/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.BASE_URL || 'http://localhost:3001/api';
+axios.defaults.baseURL = process.env.BASE_URL || '/api';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.put['Content-Type'] = 'application/json';
 
