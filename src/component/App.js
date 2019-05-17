a4a88dbb11b5b293c2cc7dba655023ef4ef3a4ee
Andrew Sutherland <azsuth@gmail.com>
2019-05-17 15:58:30 -0500

    force bypass login for testing

--- src/component/App.js
+++ src/component/App.js
@@ -11,7 +11,7 @@ import "component/style/App.css";
 
 class App extends Component {
   renderLoggedInRoutes() {
-    if (this.props.loggedIn) {
+    if (this.props.loggedIn || true) {
       return <Route path="/exercises" component={ExerciseContainer} />;
     }
 
